78b4ddb39a092867017a02aae571c07e46b314d7
Sagar <sagar.writeme@gmail.com>
2017-11-02 17:44:22 +0530

    Tests for StyleHandler

--- views/misc_test.go
+++ views/misc_test.go
@@ -5,9 +5,35 @@
 package views
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"github.com/s-gv/orangeforum/static"
 )
 
 func TestStyleHandler(t *testing.T) {
-	t.Fatalf("TODO: Write test for StyleHandler.")
+	req, err := http.NewRequest("GET", "/static/css/orangeforum.css", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(StyleHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v", status)
+	}
+
+	if body := rr.Body.String(); body != static.StyleSrc {
+		t.Errorf("handler returned unexpected body: got %s", body)
+	}
+
+	if ccHeader := rr.Header().Get("Cache-Control"); ccHeader == "" {
+		t.Errorf("Cache-Control header not set.")
+	}
+
+	if ctHeader := rr.Header().Get("Content-Type"); ctHeader != "text/css" {
+		t.Errorf("Content-Type header incorrect. Got: %s\n", ctHeader)
+	}
 }
