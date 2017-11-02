7e0157e1b2390e402a447d3e53bfd077e7671403
Sagar <sagar.writeme@gmail.com>
2017-11-02 17:29:36 +0530

    Skeleton test file

---
+++ views/misc_test.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2017 Sagar Gubbi. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package views
+
+import (
+	"testing"
+)
+
+func TestStyleHandler(t *testing.T) {
+	t.Fatalf("TODO: Write test for StyleHandler.")
+}
