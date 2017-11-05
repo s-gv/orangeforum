841afb2f901ad75a0f30ab8fccef996fdf0cab9e
Sagar Gubbi <sagar.writeme@gmail.com>
2017-11-06 01:20:59 +0530

    Still 403 Forbidden

--- views/profile_test.go
+++ views/profile_test.go
@@ -9,7 +9,72 @@ import (
 	"net/http/httptest"
 	"testing"
 	"strings"
+	"regexp"
+	"errors"
+	"net/url"
+	"fmt"
 )
+
+func grabCSRFToken(body string) (string, error) {
+	csrfToken := ""
+	r := regexp.MustCompile("<input type=\"hidden\" name=\"csrf\" value=\"([A-Za-z0-9]+)\">")
+	match := r.FindStringSubmatch(body)
+	if len(match) > 0 {
+		csrfToken = match[1]
+	}
+	if csrfToken == "" {
+		return "", errors.New("Unable to find CSRF token")
+	}
+	return csrfToken, nil
+}
+
+func grabSessionID(recorder *httptest.ResponseRecorder) (string, error) {
+	sessionid := ""
+	r := regexp.MustCompile("^sessionid=([A-Za-z0-9]+);")
+	for _, cookie := range recorder.HeaderMap["Set-Cookie"] {
+		matches := r.FindStringSubmatch(cookie)
+		if len(matches) > 0 {
+			sessionid = matches[1]
+		}
+	}
+	if sessionid == "" {
+		return "", errors.New("Unable to find sessionid")
+	}
+	return sessionid, nil
+}
+
+func loginForTest(username string, passwd string) (string, error) {
+	// GET login page
+	loginReq, _ := http.NewRequest("GET", "/login", nil)
+	loginRR := httptest.NewRecorder()
+	http.HandlerFunc(LoginHandler).ServeHTTP(loginRR, loginReq)
+
+	sessionid, serr := grabSessionID(loginRR)
+	if serr != nil {
+		return "", serr
+	}
+
+	csrfToken, cerr := grabCSRFToken(loginRR.Body.String())
+	if cerr != nil {
+		return "", cerr
+	}
+
+	fmt.Printf("body: %s\n", loginRR.Body.String())
+
+	// POST login and record sessionid
+	form := url.Values{}
+	form.Add("username", username)
+	form.Add("passwd", passwd)
+	form.Add("csrf", csrfToken)
+	loginReq, _ = http.NewRequest("POST", "/login", strings.NewReader(form.Encode()))
+	loginReq.AddCookie(&http.Cookie{Name: "sessionid", Path: "/", Value: sessionid, HttpOnly: true})
+	loginRR = httptest.NewRecorder()
+	http.HandlerFunc(LoginHandler).ServeHTTP(loginRR, loginReq)
+
+	fmt.Printf("body: %s\n", loginRR.Body.String())
+
+	return sessionid, nil
+}
 
 func TestUserProfileHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "/users?u=admin", nil)
@@ -28,4 +93,20 @@ func TestUserProfileHandler(t *testing.T) {
 	if body := rr.Body.String(); !strings.Contains(body, "admin")  {
 		t.Errorf("Profile page does not have the name of the user. Body: %s\n", body)
 	}
+}
+
+func TestLoggedInUserProfileHandler(t *testing.T) {
+	// Get the Login page and record CSRF token
+	sessionid, err := loginForTest("admin", "admin12345")
+	if err != nil {
+		t.Fatalf("%v\n", err.Error())
+	}
+	t.Errorf("sessionid: %s\n", sessionid)
+	/*
+
+	if body := loginRR.Body.String(); !strings.Contains(body, "dfasdf")  {
+		t.Errorf("Profile page does not have the name of the user. Body: %s\n", body)
+	}
+	*/
+
 }
