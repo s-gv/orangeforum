4ee6ff7847d00a0f1948b61098857ea876d4d80b
Sagar Gubbi <sagar.writeme@gmail.com>
2017-11-10 21:55:25 +0530

    SO style code blocks work

--- views/utils.go
+++ views/utils.go
@@ -53,7 +53,7 @@ var codeRe *regexp.Regexp
 func init() {
 	linkRe = regexp.MustCompile("https?://([A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-\\.]+|localhost)(:[0-9]+)?[a-zA-Z0-9@:%_\\+\\.~#?&/=;\\-]*[a-zA-Z0-9@:%_\\+~#?&/=;\\-]")
 	boldRe = regexp.MustCompile("\\*([^\\*\n]+)\\*")
-	codeRe = regexp.MustCompile("\\s\\s\\s\\s([^\\n]+)")
+	codeRe = regexp.MustCompile("    ([^\n]+)")
 }
 
 func ErrServerHandler(w http.ResponseWriter, r *http.Request) {
@@ -135,16 +135,20 @@ func validateName(name string) error {
 
 func formatComment(comment string) template.HTML {
 	comment = strings.Replace(comment, "\r", "", -1)
+
 	comment = template.HTMLEscapeString(comment)
-	log.Printf("Raw:\n%s\n", comment)
+
 	comment = codeRe.ReplaceAllString(comment, "<pre>$1</pre>")
-	log.Printf("Pre:\n%s\n", comment)
+	comment = strings.Replace(comment, "</pre>\n<pre>", "\n", -1)
+
 	comment = strings.Replace(comment, "\n\n", "</p><p>", -1)
 	comment = strings.Replace(comment, "\n", "<br>", -1)
+
 	comment = boldRe.ReplaceAllString(comment, "<b>$1</b>")
+
 	comment = linkRe.ReplaceAllString(comment, "<a href=\"$0\">$0</a>")
 
-	formatted := "<p>\n" + comment + "\n</p>"
+	formatted := "<p>" + comment + "</p>"
 	return template.HTML(formatted)
 }
 
