9c6d258c7725d656e83bd50c00757548b458f135
Sagar <sagar.writeme@gmail.com>
2017-11-11 11:51:20 +0530

    Bug fix again with code block regex

--- views/utils.go
+++ views/utils.go
@@ -56,7 +56,7 @@ func init() {
 	linkRe = regexp.MustCompile("https?://([A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-\\.]+|localhost)(:[0-9]+)?[a-zA-Z0-9@:%_\\+\\.~#?&/=;\\-]*[a-zA-Z0-9@:%_\\+~#?&/=;\\-]")
 	italicRe = regexp.MustCompile("\\*([^\\*\n]+)\\*")
 	boldRe = regexp.MustCompile("\\*\\*([^\\*\n]+)\\*\\*")
-	codeRe = regexp.MustCompile("```.*\n(?s:(.+))\n```\n?")
+	codeRe = regexp.MustCompile("(?:^|\n)```.*\n(?s:(.+))\n```(?:$|\n)")
 	oldCodeRe = regexp.MustCompile("(?:^|\n)    ([^\n]+)")
 }
 
