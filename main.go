ef673f6f4852a849690b5caba42890d942a5bda0
Sagar Gubbi <sagar.writeme@gmail.com>
2021-02-14 02:15:59 +0530

    Auth middleware

--- main.go
+++ main.go
@@ -5,13 +5,26 @@
 package main
 
 import (
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
 	"github.com/s-gv/orangeforum/models"
+	"github.com/s-gv/orangeforum/views"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	flag.Parse()
+
 	db := sqlx.MustConnect("sqlite3", "orangeforum.db?_journal_mode=WAL&_synchronous=NORMAL&_cache_size=-128000&_temp_store=2&_busy_timeout=2000")
-	models.Migrate(db)
+	models.DB = db
+	models.Migrate()
+
+	r := views.GetRouter()
+
+	glog.Info("Starting server on port 9123")
+	http.ListenAndServe(":9123", r)
 }
---
+++ views/views.go
@@ -0,0 +1,99 @@
+// Copyright (c) 2021 Orange Forum authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package views
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+	"github.com/gorilla/csrf"
+	"github.com/s-gv/orangeforum/models"
+)
+
+var tokenAuth *jwtauth.JWTAuth
+
+func GetRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(30 * time.Second))
+
+	csrfMiddleware := csrf.Protect([]byte("32-byte-long-auth-key"), csrf.Secure(false))
+	r.Use(csrfMiddleware)
+
+	sessionManager := scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
+	r.Use(sessionManager.LoadAndSave)
+
+	tokenAuth = jwtauth.New("HS256", []byte("secret1"), nil)
+
+	// Auth routes
+	r.Route("/auth", func(r chi.Router) {
+		r.Route("/signin", func(r chi.Router) {
+			r.Get("/", getAuthSignIn)
+			r.Post("/", postAuthSignIn)
+		})
+
+		r.Route("/otsignin", func(r chi.Router) {
+			r.Get("/", getAuthOneTimeSignIn)
+			r.Post("/", postAuthOneTimeSignIn)
+			r.Get("/{token}", getAuthOneTimeSignInDone)
+		})
+
+		r.Route("/signup", func(r chi.Router) {
+			r.Get("/", getAuthSignUp)
+			r.Post("/", postAuthSignUp)
+		})
+
+		r.Route("/changepass", func(r chi.Router) {
+			r.Use(jwtauth.Verifier(tokenAuth))
+			r.Use(mustAuth)
+
+			r.Get("/", getAuthChangePass)
+			r.Post("/", postAuthChangePass)
+		})
+
+		r.Route("/logout", func(r chi.Router) {
+			r.Use(jwtauth.Verifier(tokenAuth))
+			r.Use(canAuth)
+
+			r.Get("/", getAuthLogout)
+		})
+	})
+
+	r.Route("/can", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(tokenAuth))
+		r.Use(canAuth)
+
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			var user *models.User
+			if u, ok := r.Context().Value(CtxUserKey).(*models.User); ok {
+				user = u
+			}
+			w.Write([]byte(fmt.Sprintf("public area. hi %v", user)))
+		})
+	})
+
+	r.Route("/must", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(tokenAuth))
+		r.Use(mustAuth)
+
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			user := r.Context().Value(CtxUserKey).(*models.User)
+			w.Write([]byte(fmt.Sprintf("protected area. hi %v", user)))
+		})
+	})
+
+	return r
+}
