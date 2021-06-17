5a8d8c48a292ebfa4297f95e38c49a87eb914fdf
Sagar Gubbi <sagar.writeme@gmail.com>
2021-06-18 02:10:05 +0530

    Super user creation from command line

---
+++ management.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2021 Orange Forum authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func getCreds() (string, string) {
+	var userName string
+	fmt.Printf("Username: ")
+	fmt.Scanf("%s\n", &userName)
+
+	fmt.Printf("Password: ")
+	password, err := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Printf("\n")
+	if err != nil {
+		fmt.Printf("[ERROR] %s\n", err)
+		return "", ""
+	}
+	if len(password) < 8 {
+		fmt.Printf("[ERROR] Password should have at least 8 characters.\n")
+		return "", ""
+	}
+
+	fmt.Printf("Password (again): ")
+	password2, err := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Printf("\n")
+	if err != nil {
+		fmt.Printf("[ERROR] %s\n", err)
+	}
+
+	pass := string(password)
+	pass2 := string(password2)
+	if pass != pass2 {
+		fmt.Printf("[ERROR] The two psasswords do not match.\n")
+		return "", ""
+	}
+
+	return userName, pass
+}
