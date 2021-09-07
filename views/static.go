00ef57428cf5de5f86c699632b0d8413c9925898
Sagar Gubbi <sagar.writeme@gmail.com>
2021-09-07 22:01:19 +0530

    Cache static content

--- views/static.go
+++ views/static.go
@@ -12,20 +12,24 @@ import (
 
 func getCSS(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/css")
+	w.Header().Set("Cache-Control", "max-age=31536000")
 	w.Write([]byte(templates.CSSStr))
 }
 
 func getJS(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/javascript")
+	w.Header().Set("Cache-Control", "max-age=31536000")
 	w.Write([]byte(templates.JSStr))
 }
 
 func getIcon(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Cache-Control", "max-age=31536000")
 	w.Write([]byte(templates.IconStr))
 }
 
 func getLogo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Cache-Control", "max-age=31536000")
 	w.Write([]byte(templates.LogoStr))
 }
